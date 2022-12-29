acf937f70c888002bdcb20f27b28beef4a3ab0fd
small-dogg <sy920112@naver.com>
2022-12-29 23:31:50 +0900

    feat: 코틀린 맛보기

---
+++ app/src/main/kotlin/kotlin/study/model/Person.kt
@@ -0,0 +1,16 @@
+package kotlin.study.model
+
+class Person {
+    data class Person(val name: String, val age: Int? = null)
+
+    fun main(args: Array<String>) {
+        val persons = listOf(
+                Person("영희"),
+                Person("철수", 29)
+        )
+
+        val oldest = persons.maxBy { it.age ?: 0 }
+
+        println("나이가 가장 많은 사람: $oldest")
+    }
+}
