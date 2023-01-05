91d46c5ed18e6dfb39f35ce6383ca2b990f79711
small-dogg <sy920112@naver.com>
2023-01-05 23:34:52 +0900

    feat: 2장 코틀린 기초  - enum

---
+++ app/src/main/kotlin/kotlinstudy/ch02/Color01.kt
@@ -0,0 +1,5 @@
+package kotlinstudy.ch02
+
+enum class Color01 {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
---
+++ app/src/main/kotlin/kotlinstudy/ch02/Color02.kt
@@ -0,0 +1,15 @@
+package kotlinstudy.ch02
+
+enum class Color02(
+        val r: Int, val g: Int, val b: Int
+) {
+    RED(255, 0, 0),
+    ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    INDIGO(75, 0, 130),
+    VIOLET(238, 130, 238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
+}
